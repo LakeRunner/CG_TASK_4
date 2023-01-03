71fcd5ca169da91d643257d6a7bfc87c1e77e0e1
DimaKotovProgrammist <dskotov003@gmail.com>
2023-01-03 15:30:13 +0300

    add minor changes

--- src/main/java/com/cgvsu/render_engine/RenderEngine.java
+++ src/main/java/com/cgvsu/render_engine/RenderEngine.java
@@ -1,8 +1,11 @@
 package com.cgvsu.render_engine;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import com.cgvsu.GuiController;
 import com.cgvsu.math.*;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.canvas.GraphicsContext;
 import com.cgvsu.model.Model;
 import javafx.scene.paint.Color;
@@ -13,7 +16,7 @@ public class RenderEngine {
 
     public static void render(final GraphicsContext graphicsContext, final Camera camera, final Model mesh,
                               final int width, final int height, final Vector3f rotateV, final Vector3f scaleV,
-                              final Vector3f translateV) {
+                              final Vector3f translateV, Color color) {
 
         Matrix4f modelMatrix = rotateScaleTranslate(rotateV, scaleV, translateV);
         Matrix4f viewMatrix = camera.getViewMatrix();
@@ -22,14 +25,14 @@ public class RenderEngine {
         Matrix4f modelViewProjectionMatrix = new Matrix4f(projectionMatrix);
         modelViewProjectionMatrix =  Matrix4f.matrixMultiplier(modelViewProjectionMatrix, viewMatrix);
         modelViewProjectionMatrix =  Matrix4f.matrixMultiplier(modelViewProjectionMatrix, modelMatrix);
-        final int nPolygons = mesh.polygons.size();
-        graphicsContext.setStroke(Color.LIGHTGRAY);
+        final int nPolygons = mesh.getPolygons().size();
+        graphicsContext.setStroke(color);
         for (int polygonInd = 0; polygonInd < nPolygons; ++polygonInd) {
-            final int nVerticesInPolygon = mesh.polygons.get(polygonInd).getVertexIndices().size();
+            final int nVerticesInPolygon = mesh.getPolygons().get(polygonInd).getVertexIndices().size();
 
             ArrayList<Vector2f> resultPoints = new ArrayList<>();
             for (int vertexInPolygonInd = 0; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
-                Vector3f vertex = mesh.vertices.get(mesh.polygons.get(polygonInd).getVertexIndices().get(vertexInPolygonInd));
+                Vector3f vertex = mesh.getVertices().get(mesh.getPolygons().get(polygonInd).getVertexIndices().get(vertexInPolygonInd));
 
                 Vector4f vertexVecmath = new Vector4f(vertex.getX(), vertex.getY(), vertex.getZ(), 1);
 
